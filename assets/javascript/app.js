0ea1d22f4e93e7acc09a0f2c9d5c9a8244e7cfe9
Cecilia Leon <leoncecilia17@gmail.com>
2019-05-28 16:39:46 -0500

    set up of timer

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    var correctAnswers = 0;
+    var incorrectAnswers = 0;
+    var unAnswered = 0;
+    var index = 0;
+
+
+    // $("#button").on("click", function () {
+    //     $("#timer").click(function () {
+    //         var counter = 120; 
+    //         setInterval(function () {
+    //             if (counter >= 0) {
+    //                 span = document.getElementbyId("count");
+    //                 span.innerHTML = counter;
+    //             }
+
+    //             if (counter >= 0) {
+    //                 alert('sorry, out of time');
+    //                 clearInterval(counter);
+    //             } 
+    //         }, 1000);
+
+    //     });
+    // });
+
+    
+
+}); 
